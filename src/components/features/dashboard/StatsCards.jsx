2ba602e0d01d695015326c7b924964dbc3f0dc27
Mohamed_Elmasry <elmasrymohamed748123@gmail.com>
2025-11-21 23:51:56 +0400

    chore: temporary rename Card to CardTemp

--- src/components/features/dashboard/StatsCards.jsx
+++ src/components/features/dashboard/StatsCards.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card-item";
 import { LineChart, CalendarDays, DollarSign, ShoppingBag } from "lucide-react";
 
 const stats = [
--- src/components/layout/RightSidebar.jsx
+++ src/components/layout/RightSidebar.jsx
@@ -1,5 +1,5 @@
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card-item";
 
 import { Separator } from "../ui/separator";
 import CardActions from "../cards/CardActions";
